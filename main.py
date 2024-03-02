28c88e1a79956eff3dfda89eb564d3c9dabb1743
dev-shah-2204 <dgm82382@gmail.com>
2024-03-02 12:25:13 +0530

    Use blueprints

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
+from app import views
+
 
 app = Flask(__name__, static_folder="./frontend/static", template_folder="./frontend/templates")
 
-
-@app.route('/')
-def hello_world():
-    return render_template('forum.html')
+app.register_blueprint(views.views, url_prefix="/")
 
 
 if __name__ == '__main__':
