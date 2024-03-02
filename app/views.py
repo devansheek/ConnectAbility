c730c978f2c615f53a528d14b2a061f6aa686f5e
dev-shah-2204 <dgm82382@gmail.com>
2024-03-02 12:14:24 +0530

    Make views blueprint

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from app import views
+
+
+app = Flask(__name__, static_folder="./frontend/static", template_folder="./frontend/templates")
+
+app.register_blueprint(views.views, url_prefix="/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
