efde1f3b17041edba8ce9683886100f8be96fb10
devansheek <devanshee.kansara@gmail.com>
2024-03-02 11:03:18 +0530

    add frontend templates

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./frontend/static", template_folder="./frontend/templates")
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('forum.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
